ac312aa03d58dc9d58fbe7746ccc3cd52aa2f4c6
maaykeh1 <huijnenmaayke@gmail.com>
2021-09-06 15:08:19 +0000

    versie 1
    Signed-off-by: maaykeh1 <huijnenmaayke@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var x = 50;
+var y = 50;
+var speedX = 2;
+var speedY = 5;
 /**
  * setup
  * de code in deze functie wordt één keer uitgevoerd door
@@ -8,7 +12,7 @@ function setup() {
   createCanvas(1280, 720);
 
   // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
+  background(231, 199, 255;
 }
 
 
@@ -18,9 +22,20 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
+  // Kleur de achtergrond blauw, zodat je het kunt zien
+  background(231, 199, 255;
   // stel vulkleur in
   fill(100, 100, 255);
 
   // teken een cirkel
-  ellipse(50,50,80,80);
+  ellipse(x,y,80,80);
+
+  //positie van de bal
+  x = x + speedX
+  y = y + speedY
+
+  if (y === 720){
+    speedY = speedY * -1;
+  }
+
 }
