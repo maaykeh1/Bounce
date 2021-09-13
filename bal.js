fa2bab83c8caab7a2d082dbd97fa9a1a10072f26
maaykeh1 <huijnenmaayke@gmail.com>
2021-09-13 11:51:50 +0000

    les 3 (?)
    Signed-off-by: maaykeh1 <huijnenmaayke@gmail.com>

--- bal.js
+++ bal.js
@@ -1,32 +1,30 @@
-class Bal{
- x; _x
- y; _y
- speedX; 3
- speedY; 2
-
- constructor(_x, _y){
-    this.x = _x;
-    this.y = _y;
-    speedX = 3;
-    speedY = -2;
- } 
-
- show(){
-     fill(255, 0, 0);
-     ellipse (this.x, this.y, 50, 50);
- }
-
- update(){
-     this.x = this.x + this.speedX;
-     this.y = this.y + this.speedY;
-
-     if(this.x <= 0 || this.x >= 1280){
-         this.speedX = this.speedX * -1;
-     }
-
-     if(this.y <= 0 || this.y >= 720){
-        this.speedY = this.speedY * -1;
+class Bal {
+    x;
+    y;
+    speedX;
+    speedY;
+ 
+    constructor(_x, _y, _speedX, _speedY) {
+      this.x = _x;
+      this.y = _y;
+      this.speedX = _speedX;
+      this.speedY = _speedY;
     }
- }
-
-}
+ 
+    update() {
+      this.x = this.x + this.speedX;
+      this.y = this.y + this.speedY;
+ 
+      if (this.x <=0 || this.x >= 1280) {
+        this.speedX = this.speedX * -1;
+      }
+      if (this.y <=0 || this.y >= 720) {
+        this.speedY = this.speedY * -1;
+      }
+    }
+ 
+    show() {
+      fill(255, 0, 0);
+      ellipse(this.x, this.y, 50, 50);
+    }
+  }
--- script.js
+++ script.js
@@ -1,48 +1,13 @@
-var x = [50, 100, 150, 200, 250];
-var y = [50, 100, 150, 200, 250];
-var speedX = [2, 5, 15, 20, 25];
-var speedY = [5, 2, 7, 9, 12];
-/**
- * setup
- * de code in deze functie wordt één keer uitgevoerd door
- * de p5 library, zodra het spel geladen is in de browser
- */
-function setup() {
-  // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
-  createCanvas(1280, 720);
+//var x = 50;
+ //var y = 50;
+ //var speedX = 2;
+ //var speedY = 5;
+ var bal = new Bal(50, 100);
 
-}
-/**
- * draw
- * de code in deze functie wordt meerdere keren per seconde
- * uitgevoerd door de p5 library, nadat de setup functie klaar is
- */
 function draw() {
-  // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
-  // stel vulkleur in
-  fill(255, 255, 255);
-  // teken een cirkel
-  ellipse(x, y, 80, 80);
-  
-  for (let i = 0; i < x.length(); i++)
-  {
-    if(x[i] <= 0 || x[i] >= 1280)
-    {
-      speedX[i] = speedX[i] * -1;
-    }
+   // Kleur de achtergrond blauw, zodat je het kunt zien
+   background('blue');
 
-    if(y[i] <= 0 || y[i] >= 720)
-    {
-     speedY[i] = speedY[i] * -1;
-    }
-  }
-
-  //positie updaten 
- // x = x + speedX;
- // y = y + speedY;
-  // laat stuiteren tegen de onderkant
- // if (y === 720) {
-  //  speedY = speedY * -1;
- // } 
-}
+   bal.show();
+   bal.update();
+ } 
